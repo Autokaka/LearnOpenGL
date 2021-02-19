537ed0f7f81e53f7a8bbc67372c837e79974a336
luao <luao@bilibili.com>
2021-02-19 13:55:01 +0800

    passing vertexColor from vertexShader to fragmentShader
    in order to draw a red triangle

--- openGLPractice/openGLPractice/main.cpp
+++ openGLPractice/openGLPractice/main.cpp
@@ -51,16 +51,19 @@ int main() {
     // ------------------------------------
     const char *vertexShaderSource = "#version 330 core \n"
         "layout (location = 0) in vec3 aPos;\n"
+        "out vec4 vertexColor;\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   vertexColor = vec4(0.5, 0, 0, 1.0);\n"
         "}\0";
     const char *fragmentShaderSource = "#version 330 core\n"
+        "in vec4 vertexColor;\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
+        "   FragColor = vertexColor;\n"
+        "}\0";
     // vertex shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
