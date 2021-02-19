cde59579af64a7572008dd7864f29aa33e8754e5
luao <luao@bilibili.com>
2021-02-19 17:07:52 +0800

    pass position as color data from vertex shader to fragment shader
    task answer:
    since position coordinate that are less than 0.0f (-0.5f from example)
    will be set to 0.0f, the bottom left point (-0.5f, -0.5f, 0.0f) will
    be set to (0.0f, 0.0f, 0.0f) in ourColor, which means FragColor will
    be (0.0f, 0.0f, 0.0f, 1.0f), known as pure black.

--- openGLPractice/openGLPractice/3.3.shader.fs
+++ openGLPractice/openGLPractice/3.3.shader.fs
@@ -4,6 +4,10 @@ in vec3 ourColor;
 
 out vec4 FragColor;
 
+// since position coordinate that are less than 0.0f (-0.5f from example)
+// will be set to 0.0f, the bottom left point (-0.5f, -0.5f, 0.0f) will
+// be set to (0.0f, 0.0f, 0.0f) in ourColor, which means FragColor will
+// be (0.0f, 0.0f, 0.0f, 1.0f), known as pure black.
 void main() {
     FragColor = vec4(ourColor, 1.0);
 }
