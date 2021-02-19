8fa3bcd11eb62d1f52ead4b8b3affa8df3d248ab
luao <luao@bilibili.com>
2021-02-19 16:43:59 +0800

    customize your own shader!

--- openGLPractice/openGLPractice/main.cpp
+++ openGLPractice/openGLPractice/main.cpp
@@ -10,6 +10,8 @@
 
 #include <iostream>
 
+#include "shader.hpp"
+
 void frame_buffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -49,54 +51,7 @@ int main() {
     
     // build and compile our shader program
     // ------------------------------------
-    const char *vertexShaderSource = "#version 330 core \n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "out vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos, 1.0);\n"
-        "   ourColor = aColor;\n"
-        "}\0";
-    const char *fragmentShaderSource = "#version 330 core\n"
-        "in vec3 ourColor;\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(ourColor, 1.0);\n"
-        "}\0";
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader ourShader("/complete/path/to/3.3.shader.vs", "/complete/path/to/3.3.shader.fs");
     
     // set up vertex data (and buffer(s))
     // and configure vertex attributes
@@ -136,15 +91,9 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
         
         // draw triangle
-        glUseProgram(shaderProgram);
+        ourShader.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        
-        // update shader uniform
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         
         // glfw: swap buffers and
         // poll IO events (keys
@@ -158,7 +107,6 @@ int main() {
     // ---------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
---
+++ openGLPractice/openGLPractice/shader.hpp
@@ -0,0 +1,29 @@
+//
+//  shader.hpp
+//  openGLPractice
+//
+//  Created by luao on 2021/2/19.
+//
+
+#ifndef shader_hpp
+#define shader_hpp
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <glad/glad.h>
+
+class Shader {
+public:
+    unsigned int ID;
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use() const;
+    // tools to set uniform value
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+#endif /* shader_hpp */
